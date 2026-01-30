410eee626fbc3d7d3d75b096ebf7d906653bb9ad
guitch95 <guitch95@gmail.com>
2026-01-30 15:13:12 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Guillaume Richard on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
