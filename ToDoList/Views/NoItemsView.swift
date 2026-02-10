67f8250786bb22e792720167b42167a9aac8849d
Guillaume Richard <guitch95@gmail.com>
2026-02-10 13:49:32 +0100

    Created the NoItemView to display when there is no notes registered.

---
+++ ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Guillaume Richard on 10/02/2026.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
